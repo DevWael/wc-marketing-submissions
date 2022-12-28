7a30fbd5a4686b4a4ba297473e9c4581c3c9851a
devwael <dev.ahmedwael@gmail.com>
2022-12-28 15:57:42 +0200

    saving submission on checkout

--- Modules/Checkout.php
+++ Modules/Checkout.php
@@ -14,12 +14,26 @@ class Checkout {
 			'class'       => array( 'form-row privacy' ),
 			'label_class' => array( 'woocommerce-form__label woocommerce-form__label-for-checkbox checkbox' ),
 			'input_class' => array( 'woocommerce-form__input woocommerce-form__input-checkbox input-checkbox' ),
-			'required' => false,
+			'required'    => false,
 			'label'       => 'Marketing Opt-In',
 		) );
 	}
 
 	public function process() {
-		//todo: to be implemented
+		$current_user_id = get_current_user_id();
+		if ( ! $current_user_id ) {
+			return;
+		}
+		$submission = new Submissions( $current_user_id );
+
+		if ( isset( $_POST['wms-opt-in'] ) ) {
+			if ( ! $submission->is_submission_exist() ) {
+				$submission->add_submission();
+			}
+		} else {
+			if ( $submission->is_submission_exist() ) {
+				$submission->delete_submission();
+			}
+		}
 	}
 }
