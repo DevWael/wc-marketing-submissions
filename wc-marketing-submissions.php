f551b7af3047419ef022e308c3c229d8f4279110
devwael <dev.ahmedwael@gmail.com>
2022-12-28 13:51:17 +0200

    initial files

---
+++ wc-marketing-submissions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WC Marketing Submissions
+ * Plugin URI: https://deedy.uk
+ * Description: Add option to opt-in customers into marketing lists emails.
+ * Version: 1.0
+ * Author: Ahmad Wael
+ * Author URI: https://bbioon.com
+ * Text Domain: wms
+ * Domain Path: /languages/
+ */
