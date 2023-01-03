0c5809ec6e38d44f1595cb4e0ff5ec5a05521646
devwael <dev.ahmedwael@gmail.com>
2023-01-03 19:55:37 +0200

    add metabox data to single submission

--- Modules/Post_Type.php
+++ Modules/Post_Type.php
@@ -5,6 +5,7 @@ class Post_Type {
 
 	public function setup_hooks() {
 		add_action( 'init', [ $this, 'cpt' ] );
+		add_filter( 'add_meta_boxes', [ $this, 'metabox' ] );
 		add_filter( 'manage_wms-entries_posts_columns', [ $this, 'columns' ] );
 		add_action( 'manage_wms-entries_posts_custom_column', [ $this, 'user_id_column' ], 10, 2 );
 	}
@@ -25,7 +26,6 @@ class Post_Type {
 			'not_found'          => __( 'Not Found', 'wms' ),
 			'not_found_in_trash' => __( 'Not found in Trash', 'wms' ),
 		);
-// Set other options for Custom Post Type
 
 		$args = array(
 			'label'               => __( 'Submissions', 'wms' ),
@@ -39,6 +39,7 @@ class Post_Type {
 			*/
 			'hierarchical'        => false,
 			'public'              => false,
+			'menu_icon'           => 'dashicons-feedback',
 			'show_ui'             => true,
 			'show_in_menu'        => true,
 			'show_in_nav_menus'   => true,
@@ -56,8 +57,49 @@ class Post_Type {
 			'show_in_rest'        => true,
 		);
 
+		$args = apply_filters( 'wms_post_type_args', $args );
 		// Registering your Custom Post Type
 		register_post_type( 'wms-entries', $args );
+	}
+
+	public function metabox() {
+		add_meta_box( 'wms', __( 'Submission Details', 'wms' ), [ $this, 'metabox_content' ], 'wms-entries' );
+	}
+
+	public function metabox_content( $post ) {
+		$user_id = get_post_meta( $post->ID, 'wms_user_id', true );
+		$user    = get_user_by( 'ID', $user_id );
+		do_action( 'wms_before_metabox_content', $post, $post->ID );
+		?>
+        <table class="form-table" role="presentation">
+            <tr>
+                <th scope="row"><label for="user_id"><?php _e( 'User ID', 'wms' ); ?></label></th>
+                <td><input readonly name="user_id" type="text" id="user_id"
+                           value="<?php echo esc_attr( $user_id ?: '---' ); ?>"
+                           class="regular-text"></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="username"><?php _e( 'Username', 'wms' ); ?></label></th>
+                <td><input readonly name="username" type="text" id="username"
+                           value="<?php echo $user ? esc_html( $user->user_login ) : '---'; ?>"
+                           class="regular-text"></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="fullname"><?php _e( 'Full name', 'wms' ); ?></label></th>
+                <td><input readonly name="fullname" type="text" id="fullname" value="<?php
+					echo esc_attr( get_post_meta( $post->ID, 'wms_user_first_name', true ) ) . ' ' .
+					     esc_attr( get_post_meta( $post->ID, 'wms_user_last_name', true ) ); ?>"
+                           class="regular-text"></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="email_address"><?php _e( 'Email address', 'wms' ); ?></label></th>
+                <td><input readonly name="email_address" type="text" id="email_address"
+                           value="<?php echo esc_attr( get_post_meta( $post->ID, 'wms_user_email', true ) ); ?>"
+                           class="regular-text"></td>
+            </tr>
+        </table>
+		<?php
+		do_action( 'wms_after_metabox_content', $post, $post->ID );
 	}
 
 	public function columns( $columns ) {
